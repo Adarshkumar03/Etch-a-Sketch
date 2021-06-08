7d00bd7ae249941bc63f4156e2fbbcc3a2cfd7af
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-08 12:04:14 +0530

    fin

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("#clear");
+const clearBtn = document.querySelector("#clear");
+const setSizeBtn = document.querySelector("#set-size");
+const setBlack = document.querySelector("#black");
+
+var isBlack = false;
 
 function makeGrid(rows, cols) {
-    container.style.setProperty("--grid-row", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for(let c = 0; c < (rows*cols); c++){
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-    }
+    let gridArea = rows * cols;
+    for (let i = 1; i <= gridArea; i++) {
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
+        container.insertAdjacentElement('beforeend', gridItem);
+    } 
+    var gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => {
+        gridPixel.addEventListener("mouseover", changeColor);
+    });
 }
+   
 makeGrid(16,16);
 
-const gridItems = document.querySelectorAll(".grid-item");
+
 
 function clearColor(){
-    gridItems.forEach(item => {
+    let gridList = document.querySelectorAll("div");
+    gridList.forEach(item => {
         item.style.backgroundColor = "";
     });    
 }
 
+function convert(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+    function hexCode(i) {
+        return ("0" + parseInt(i).toString(16)).slice(-2);
+    }
+    return "#" + hexCode(rgb[1]) + hexCode(rgb[2])
+            + hexCode(rgb[3]);
+}
+
 function changeColor(e) {
-    if(e.target.style.backgroundColor===""){
+    if(isBlack){
+        e.target.style.backgroundColor = "black";
+    }else{
     const r = Math.floor(Math.random()*256);
     const g = Math.floor(Math.random()*256);
     const b = Math.floor(Math.random()*256);
     e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
-    }else{
-        let color = e.target.backgroundColor;
-
     }
 }
 
-gridItems.forEach(item => {
-    item.addEventListener("mouseover", changeColor)
-})
+function setSize() {
+    let size = prompt("Enter the size(Maximum:50)", "16");
+    let gridPixels = container.querySelectorAll('.grid-item');
+    gridPixels.forEach(gridPixel => gridPixel.remove())
+    makeGrid(size,size);
+}
 
-button.addEventListener("click", clearColor);
+
+setBlack.addEventListener("click", ()=>{
+    isBlack=!isBlack;
+})
+clearBtn.addEventListener("click", clearColor);
+setSizeBtn.addEventListener("click", setSize);
